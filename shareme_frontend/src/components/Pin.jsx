225a959a70c39e563bb8fdf594f7592a2ed011a3
rb718 <ghisodye@hotmail.com>
2023-03-21 03:22:24 -0500

    PinDetail done

--- shareme_frontend/src/components/Pin.jsx
+++ shareme_frontend/src/components/Pin.jsx
@@ -102,9 +102,9 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                   className="bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:100 hover:shadow-md"
                 >
                   <BsFillArrowUpRightCircleFill />
-                  {destination.length > 20
-                    ? destination.slice(8, 20)
-                    : destination.slice(8)}
+                  {destination.length > 15
+                    ? `${destination.slice(0, 15)}...`
+                    : destination}
                 </a>
               )}
               {postedBy?._id === user.id && (
